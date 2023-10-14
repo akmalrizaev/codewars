c3ea627ed075326c3d668f23bb29d25bac8d9e11
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-14 21:40:17 +0500

    Convert boolean values to strings

---
+++ 8-kyu/bool-to-word.js
@@ -0,0 +1,13 @@
+// Description:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+  }
+  
+  
+  console.log(boolToWord(true));  // Output: 'Yes'
+  console.log(boolToWord(false)); // Output: 'No'
+  
