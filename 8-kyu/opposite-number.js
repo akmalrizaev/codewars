74bf01ef80ee83b4ae5c772bebcc7391dcd43d9a
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-15 06:24:36 +0500

    Opposite number

---
+++ 8-kyu/opposite-number.js
@@ -0,0 +1,17 @@
+// Description:
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    return -number;
+  }
+
+  console.log(oppositeNumber(1));   // Output: -1
+console.log(oppositeNumber(14));  // Output: -14
+console.log(oppositeNumber(-34)); // Output: 34
