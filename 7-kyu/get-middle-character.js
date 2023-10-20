2de33b4bf93baab674e28aa597c2ae722be38394
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-20 06:32:55 +0500

    Get the middle number

---
+++ 7-kyu/get-middle-character.js
@@ -0,0 +1,41 @@
+// Description:
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+function getMiddle(s)
+{
+ const length = s.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length % 2 === 0) {
+    // Word has an even length, so return the middle 2 characters.
+    return s.slice(middleIndex - 1, middleIndex + 1);
+  } else {
+    // Word has an odd length, so return the middle character.
+    return s[middleIndex];
+  }
+}
+
+
+console.log(getMiddle("test"));     // Output: "es"
+console.log(getMiddle("testing"));  // Output: "t"
+console.log(getMiddle("middle"));   // Output: "dd"
+console.log(getMiddle("A"));        // Output: "A"
